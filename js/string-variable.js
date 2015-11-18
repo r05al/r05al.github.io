5775ea2d22bcd00847af5e36af6d384ed8eaaee6
Peter Rosal <peter.rosal@gmail.com>
2015-11-18 16:04:52 -0500

    Remove old files with updated single page set up

--- js/string-variable.js
+++
@@ -1,9 +0,0 @@
-var username;
-var message;
-username = 'Phase 1***';
-message = '';
-
-var elName = document.getElementById('name');
-elName.textContent = username;
-var elNote = document.getElementById('note');
-elNote.textContent = message;
